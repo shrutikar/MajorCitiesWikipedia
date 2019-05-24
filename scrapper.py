7eaa71b0998d1da6655892e1afe435eceee9768c
shrutikar <kar.3@wright.edu>
2019-05-23 20:07:39 -0700

    cities and links

--- scrapper.py
+++ scrapper.py
@@ -8,22 +8,21 @@ soup = BeautifulSoup(website_url,"lxml")
 # print(soup.prettify())
 My_table = soup.find("table",{"class":"wikitable sortable"})
 # print(My_table)
-links = My_table.find_all("a")
-# print(links)
-City=[]
+links = My_table
+i=0
+City= []
 Link=[]
-for link in links:
-    if link.get("title"):
-        City.append(link.get("title"))
-        Link.append(link.get("href"))
+for link in links.findAll('tr'):
+    if link.findAll('td'):
+        City.append(str(link.findAll('td')[1].find_all('a')[0].get("title")))
+        Link.append(str(link.findAll('td')[1].find_all('a')[0].get("href")))
 
 df=pd.DataFrame()
 df['City']=City
-
 print(df)
 
-# for link in Link:
-#     url = requests.get("https://en.wikipedia.org"+link).text
-#     soup = BeautifulSoup(url, "lxml")
-#     print(soup)
-#     break
+for link in Link:
+    url = requests.get("https://en.wikipedia.org"+link).text
+    soup = BeautifulSoup(url, "lxml")
+    print(soup)
+    break
