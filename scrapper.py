656498dcc4ad962b60d26b8778d360850e0a916c
shrutikar <kar.3@wright.edu>
2019-05-29 16:11:02 -0700

    final clean

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,6 @@ import requests, re
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 def web_scrapper(URL):
     website_url = requests.get(URL).text
     soup = BeautifulSoup(website_url, "lxml")
@@ -12,7 +11,7 @@ def clean_location(loc):
     return re.sub(r"[^a-zA-Z0-9.]", "", loc)
 
 def clean_unit(data):
-    return re.sub(r"[^0-9]", "", data)
+    return re.sub(r"[^0-9.]", "", data)
 
 def clean_change(chg):
     chg.replace(u'\u2212', '-')
@@ -33,7 +32,6 @@ def clean_header(string):
     else:
         return string
 
-
 def rearrange(df):
     d = df.columns.tolist()
     r = d.index('rank')
@@ -51,9 +49,10 @@ def redundant_columns(s):
     elif "airport" in s:
         return "airport"
     else:
-        old={'city statu':'city status','consolidated':'consolidated city-county','foundation':'founded','founded a':'founded by',
-             'gni feature id':'gni id','zip code prefixe':'zip code','zip codes':'zip code','assemblymembers':'assembly member',
-             'assembly':'assembly member','assembly members':'assembly member','assemblymember':'assembly member','counties':'county',
+        old={'city statu':'city status','consolidated':'consolidated city-county','foundation':'founded',
+             'founded a':'founded by','gni feature id':'gni id','zip code prefixe':'zip code',
+             'zip codes':'zip code','assemblymembers':'assembly member','assembly':'assembly member',
+             'assembly members':'assembly member','assemblymember':'assembly member','counties':'county',
              '[[federal infor\nmation processing standards|fip code]]':'fip code','fip code\n0':'fip code'}
 
         value = old.get(s)
@@ -64,159 +63,183 @@ def drop_less_informative_columns(df):
     b=df.isna().sum()[df.isna().sum()> 0]/len(df)
     cols=b[b>threshold].index
     df.drop(cols,inplace=True,axis=1)
-    df.drop(['density','estimate','area code'], inplace=True, axis=1) #redundant with other existing column
+    df.drop(['density','estimate','area code','zip code'], inplace=True, axis=1) #redundant with other existing column
     return df
 
-wiki = "https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population"
-soup = web_scrapper(wiki)
-table = soup.find("table",{"class":"wikitable sortable"})
+def remove_brackets(lst1):
+    new_lst1=[]
+    for e1 in lst1:
+        if str(type(e1)) != 'nan':
+            e1 = re.sub(r" ?\[[^)]+\]", "", str(e1)).strip()
+            e1 = re.sub(r" ?\([^)]+\)", "", e1).strip()
+        new_lst1.append(e1)
+    return new_lst1
 
-completedata=[]
-City= []
-Link=[]
-d=0
-for link in table.findAll('tr'):
-    col_headers = {
-        'rank': None,
-        'city': None,
-        'state': None,
-        'Estimate2018': None,
-        'Census2010': None,
-        'Change': None,
-        'Land Area2016 sqmi': None,
-        'Land Area2016 sqkm': None,
-        'Population Density2016 sqmi': None,
-        'Population Density2016 sqkm': None,
-        'Latitude': None,
-        'Longitude': None
-    }
-    if link.findAll('td'):
-        for i,(k,v) in enumerate(col_headers.items()):
-            if i==2:
-                col_headers[k]=clean_string(link.findAll('td')[i].findAll(text=True)[1])
-            elif i==5:
-                col_headers[k]=clean_change(link.findAll('td')[i].find(text=True))
-            elif i in [6,7,8,9]:
-                col_headers[k]=clean_unit(link.findAll('td')[i].find(text=True))
-            elif i==10 or i==11:
-                col_headers[k]=clean_location(link.findAll('td')[10].findAll(text=True)[4].split(" ")[i%10])
-            elif i==1:
-                col_headers[k]=link.findAll('td')[i].find(text=True)
-            else:
-                col_headers[k]=clean_unit(link.findAll('td')[i].find(text=True))
+def clean_unit_col(lst2):
+    new_lst2=[]
+    for e2 in lst2:
+        if "-" in e2:
+            e2= e2.split("-")[1]
+        if "%" in e2:
+            e2 = e2.split(" ")[0]
+        new_lst2.append(re.sub(r"[^0-9.]", "", e2))
+    return new_lst2
 
-        lnk = str(link.findAll('td')[1].find_all('a')[0].get("href").replace("%E2%80%93","-"))
-        # Link.append(lnk)
-        soup = web_scrapper('https://en.wikipedia.org/'+lnk)
-        city = soup.find("table", {"class": "infobox"})
-        for row in city.findAll('tr'):
-            if row.findAll('td'):
-                if len(row.findAll('th')) == 1 and len(row.findAll('td')) == 1:
-                    h = clean_header(row.findAll('th')[0].text)
-                    dat = row.findAll('td')[0].text.strip()
-                    if col_headers.get(h):
-                        continue
-                    else:
-                        col_headers[h]=dat
-        completedata.append(col_headers)
+def float_format(lst3):
+    new_lst3=[]
+    for e3 in lst3:
+        if e3.strip() == '.':
+            e3 = ""
+        if e3.strip():
+            e3 = float(e3)
+        new_lst3.append(e3)
+    return new_lst3
 
-    if d>10:
-        break
-    else:
-        d+=1
+def scrap_wikipedia(table2):
+    completedata2 = []
+    for link in table2.findAll('tr'):
+        col_headers = {
+            'rank': None,
+            'city': None,
+            'state': None,
+            'Estimate2018': None,
+            'Census2010': None,
+            'Change': None,
+            'Land Area2016 sqmi': None,
+            'Land Area2016 sqkm': None,
+            'Population Density2016 sqmi': None,
+            'Population Density2016 sqkm': None,
+            'Latitude': None,
+            'Longitude': None
+        }
+        if link.findAll('td'):
+            for i, (k, v) in enumerate(col_headers.items()):
+                if i == 2:
+                    col_headers[k] = clean_string(link.findAll('td')[i].findAll(text=True)[1])
+                elif i == 5:
+                    col_headers[k] = clean_change(link.findAll('td')[i].find(text=True))
+                elif i in [6, 7, 8, 9]:
+                    col_headers[k] = clean_unit(link.findAll('td')[i].find(text=True))
+                elif i == 10 or i == 11:
+                    col_headers[k] = clean_location(link.findAll('td')[10].findAll(text=True)[4].split(" ")[i % 10])
+                elif i == 1:
+                    col_headers[k] = link.findAll('td')[i].find(text=True)
+                else:
+                    col_headers[k] = clean_unit(link.findAll('td')[i].find(text=True))
 
+            lnk2 = str(link.findAll('td')[1].find_all('a')[0].get("href").replace("%E2%80%93", "-"))
+            soup2 = web_scrapper('https://en.wikipedia.org/' + lnk2)
+            city = soup2.find("table", {"class": "infobox"})
+            for row in city.findAll('tr'):
+                if row.findAll('td'):
+                    if len(row.findAll('th')) == 1 and len(row.findAll('td')) == 1:
+                        h = clean_header(row.findAll('th')[0].text)
+                        dat = row.findAll('td')[0].text.strip()
+                        if col_headers.get(h):
+                            continue
+                        else:
+                            col_headers[h] = dat
+            completedata2.append(col_headers)
+    return completedata2
 
-df2=pd.DataFrame(completedata)
+def scrap_page(COL2):
+    COL_body2 = COL2.find('tbody')
+    COL_rows2 = COL_body2.find_all('tr')
+    living2 = {}
+    for col_row2 in COL_rows2:
+        cty2 = col_row2.findAll('td')[1].text
+        data2 = col_row2.findAll('td')[2].text
+        # header = col_row.findAll('th')
+        if cty2.split(",")[-1].lower().strip() == 'united states':
+            if cty2.split(",")[0] in df2["city"].tolist():
+                living[cty2.split(",")[0]] = data2
+            elif cty2.split(",")[0].lower().strip() == 'new york':
+                living2['New York City'] = data2
+    return living2
 
-df2 = rearrange(df2)
-df2 = drop_less_informative_columns(df2)
+def scrap_other(COL3):
+    COL_body3 = COL3.findAll('li')
+    traffic3 = {}
+    for lst3 in COL_body3:
+        raw = lst3.text.split(",")
+        cty = raw[0]
+        hours = raw[1].split(":")[1][:-3].strip("hours").strip()
+        if cty in df2["city"].tolist():
+            traffic3[cty] = hours
+    return traffic3
 
-print(df2.columns.tolist())
+if __name__=='__main__':
 
+    wiki = "https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population"
+    soup = web_scrapper(wiki)
+    table = soup.find("table",{"class":"wikitable sortable"})
+    completedata = scrap_wikipedia(table)
+    df2=pd.DataFrame(completedata)
+    df2 = rearrange(df2)
+    df2 = drop_less_informative_columns(df2)
 
-#to be cleaned : city, state , rank appearing twice
-#to be cleaned things in bracket mayor , incorporated, metro, type, water, zip code
+    #Additional data : Cost of Living in major cities
+    #This column may contain null values [Why? Refer to ReadMe].
 
-new_data = {}
-print(df2["city"])
-soup = web_scrapper('https://www.numbeo.com/cost-of-living/region_rankings.jsp?title=2019&region=021')
-COL = soup.find("table", {"id": "t2"})
-COL_body = COL.find('tbody')
-COL_rows = COL_body.find_all('tr')
-living = {}
-for col_row in COL_rows:
-    cty = col_row.findAll('td')[1].text
-    data = col_row.findAll('td')[2].text
-    # header = col_row.findAll('th')
-    if cty.split(",")[-1].lower().strip() == 'united states' :
-        if cty.split(",")[0] in df2["city"].tolist():
-            living[cty.split(",")[0]]=data
-        elif cty.split(",")[0].lower().strip()== 'new york':
-            living['New York City']=data
+    soup = web_scrapper('https://www.numbeo.com/cost-of-living/region_rankings.jsp?title=2019&region=021')
+    COL = soup.find("table", {"id": "t2"})
+    living= scrap_page(COL)
+    lst=[]
+    for e in df2["city"].tolist():
+        lst.append(living.get(e))
+    df2["Cost of living Index"] = lst
 
-lst=[]
-for e in df2["city"].tolist():
-    lst.append(living.get(e))
+    #Additional Data: Average annual hours spent in traffic
+    #Non conventional database
+    #non table data
+    lnk = 'https://www.forbes.com/sites/jimgorzelany/2019/02/11/here-are-the-u-s-cities-' \
+          'suffering-the-worst-traffic-congestion/#1506f0e36e36'
+    soup = web_scrapper(lnk)
+    COL = soup.find("ol")
+    traffic=scrap_other(COL)
+    new_lst=[]
+    for e in df2["city"].tolist():
+        new_lst.append(traffic.get(e))
+    df2["Average annual traffic hours"] = new_lst
 
-df2["Cost of living"] = lst
-df2.to_csv("MajorCities.csv", index=False, encoding='utf-8-sig')
+    # Cleaning columns data changing formats
 
+    brack_cols=['mayor','elevation','type','water','metro','land']
+    for c in brack_cols:
+        df2[c] = remove_brackets(df2[c].tolist())
 
-# data = pd.read_html(str(table))
-#
-# df = pd.concat(data)
-# # print(df.columns.values)
-# # print(data[0].columns.values)
-# df.columns=cols
-# df.to_csv("try1.csv",index=False,encoding='utf-8-sig')
-# print(df['2018 estimate'])
-#
-# information ={
-#     'postalCode' : [],
-#     'areaCode' : [],
-#     'areaLand' : [],
-#     'areaWater' : [],
-#     'elevation' : [],
-#     'populationTotalRanking' : [],
-#     'utcOffset' : [],
-#     'areaTotal' : [],
-#     'governmentType' : [],
-#     'leaderTitle' : [],
-#     'populationDensity' : [],
-#     'populationTotal' : [],
-#     # 'establishedDate' : [],
-#     # 'latd': [],
-#     # 'longd': [],
-#     'areaMetro' : [],
-#     'timeZone' : []
-# }
+    unit_cols=['elevation','land','metro','water','rank','Census2010','Estimate2018','Land Area2016 sqkm',
+               'Land Area2016 sqmi','Population Density2016 sqkm','Population Density2016 sqmi']
+    for c in unit_cols:
+        df2[c] = clean_unit_col(df2[c].tolist())
+        print(c)
+        df2[c] = float_format(df2[c].tolist())
 
+    df2.to_csv("MajorCities.csv", index=False, encoding='utf-8-sig')
 
+    # Major cities Data from DBpedia
+    # DBpedia was explored as well but turns out, there are much recent and updated information on other pages.
 
-
-
-
-# # Major cities Data from DBpedia
-# for link in Link:
-#     data = requests.get('http://dbpedia.org/data/'+link[6:]+'.json').json()
-#     uri = data['http://dbpedia.org/resource/'+link[6:]]
-#     for k,v in information.items():
-#         if 'http://dbpedia.org/ontology/'+k in uri and k=='areaCode':
-#             temp = str(uri['http://dbpedia.org/ontology/' + k][0]['value'])\
-#                 .replace("&minus;", "-").replace(":00", "").replace("−", "-")\
-#                 .replace("&",",").replace(",",", ").replace("/",", ").replace("and",", ")
-#             temp = re.sub("[^0-9^,^ ]","",temp)
-#             information[k].append(temp)
-#
-#         elif 'http://dbpedia.org/ontology/'+k in uri and k != 'timeZone' and k != 'governmentType':
-#             information[k].append(str(uri['http://dbpedia.org/ontology/' + k][0]['value'])
-#                                   # .replace("&",",").replace("and",",").replace("/",",")
-#                                   .replace("&minus;","-").replace(":00","").replace("−","-").replace("−", "-").replace("−0", "-"))
-#         elif 'http://dbpedia.org/ontology/'+k in uri and (k == 'governmentType' or k == 'timeZone'):
-#             information[k].append(uri['http://dbpedia.org/ontology/' + k][0]['value'][28:].replace("–"," ").replace("_"," "))
-#         else:
-#             information[k].append("N/A")
-# for k,v in information.items():
-#     df[k]=v
-#
-# df.to_csv("DBpediaMajorCities.csv",index=False,encoding='utf-8-sig')
+    # for link in Link:
+    #     data = requests.get('http://dbpedia.org/data/'+link[6:]+'.json').json()
+    #     uri = data['http://dbpedia.org/resource/'+link[6:]]
+    #     for k,v in information.items():
+    #         if 'http://dbpedia.org/ontology/'+k in uri and k=='areaCode':
+    #             temp = str(uri['http://dbpedia.org/ontology/' + k][0]['value'])\
+    #                 .replace("&minus;", "-").replace(":00", "").replace("−", "-")\
+    #                 .replace("&",",").replace(",",", ").replace("/",", ").replace("and",", ")
+    #             temp = re.sub("[^0-9^,^ ]","",temp)
+    #             information[k].append(temp)
+    #
+    #         elif 'http://dbpedia.org/ontology/'+k in uri and k != 'timeZone' and k != 'governmentType':
+    #             information[k].append(str(uri['http://dbpedia.org/ontology/' + k][0]['value'])
+    #                                   # .replace("&",",").replace("and",",").replace("/",",")
+    #                                   .replace("&minus;","-").replace(":00","").replace("−","-").replace("−", "-").replace("−0", "-"))
+    #         elif 'http://dbpedia.org/ontology/'+k in uri and (k == 'governmentType' or k == 'timeZone'):
+    #             information[k].append(uri['http://dbpedia.org/ontology/' + k][0]['value'][28:].replace("–"," ").replace("_"," "))
+    #         else:
+    #             information[k].append("N/A")
+    # for k,v in information.items():
+    #     df[k]=v
+    #
+    # df.to_csv("DBpediaMajorCities.csv",index=False,encoding='utf-8-sig')
